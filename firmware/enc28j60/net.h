a2b826ff4b5e398c9b539853208073eb0b5fccff
Martin Heistermann <mh-git@sponc.de>
2012-06-21 00:04:11 +0200

    move ether-l0dable's network constants and headers to enc28j60/net.h

---
+++ firmware/enc28j60/net.h
@@ -0,0 +1,41 @@
+// packet structures and defines taken from / based on uip 1.0
+
+#define UIP_ETHTYPE_ARP 0x0806
+#define UIP_ETHTYPE_IP  0x0800
+#define UIP_ETHTYPE_IP6 0x86dd
+#define UIP_PROTO_ICMP  1
+#define UIP_PROTO_TCP   6
+#define UIP_PROTO_UDP   17
+#define UIP_PROTO_ICMP6 58
+
+#define HTONS(n) (uint16_t)((((uint16_t) (n)) << 8) | (((uint16_t) (n)) >> 8))
+
+struct __attribute__ ((packed)) eth_hdr {
+  uint8_t dest[8];
+  uint8_t src[8];
+  uint16_t type;
+};
+
+struct __attribute__((packed)) ip_hdr {
+    /* IP header. */
+    uint8_t vhl,
+         tos,
+         len[2],
+         ipid[2],
+         ipoffset[2],
+         ttl,
+         proto;
+    uint16_t ipchksum;
+    uint16_t srcipaddr[2],
+          destipaddr[2];
+};
+
+struct __attribute__((packed)) udp_hdr {
+    /* UDP header. */
+    uint16_t srcport,
+          destport;
+    uint16_t udplen;
+    uint16_t udpchksum;
+};
+
+
--- firmware/l0dable/ether.c
+++ firmware/l0dable/ether.c
@@ -15,57 +15,10 @@
 
 
 #include "enc28j60/enc28j60.h"
+#include "enc28j60/net.h"
 #include "funk/nrf24l01p.h"
 
 #include "usetable.h"
-
-
-// packet structures and defines taken from / based on uip 1.0
-
-#define UIP_ETHTYPE_ARP 0x0806
-#define UIP_ETHTYPE_IP  0x0800
-#define UIP_ETHTYPE_IP6 0x86dd
-#define UIP_PROTO_ICMP  1
-#define UIP_PROTO_TCP   6
-#define UIP_PROTO_UDP   17
-#define UIP_PROTO_ICMP6 58
-
-#ifndef HTONS
-#   if UIP_BYTE_ORDER == UIP_BIG_ENDIAN
-#      define HTONS(n) (n)
-#   else /* UIP_BYTE_ORDER == UIP_BIG_ENDIAN */
-#      define HTONS(n) (u16_t)((((u16_t) (n)) << 8) | (((u16_t) (n)) >> 8))
-#   endif /* UIP_BYTE_ORDER == UIP_BIG_ENDIAN */
-#endif
-
-struct __attribute__ ((packed)) eth_hdr {
-  uint8_t dest[8];
-  uint8_t src[8];
-  uint16_t type;
-};
-
-struct __attribute__((packed)) ip_hdr {
-    /* IP header. */
-    uint8_t vhl,
-         tos,
-         len[2],
-         ipid[2],
-         ipoffset[2],
-         ttl,
-         proto;
-    uint16_t ipchksum;
-    uint16_t srcipaddr[2],
-          destipaddr[2];
-};
-
-struct __attribute__((packed)) udp_hdr {
-    /* UDP header. */
-    uint16_t srcport,
-          destport;
-    uint16_t udplen;
-    uint16_t udpchksum;
-};
-
 
 
 static const struct MENU mainmenu;
