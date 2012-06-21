07d131383bc7e82fb7ac6bd76a05929dbd1e9f59
Martin Heistermann <mh-git@sponc.de>
2012-06-21 22:38:27 +0200

    net.h: fix stupid bug, mac address length was wrong

--- firmware/enc28j60/net.h
+++ firmware/enc28j60/net.h
@@ -11,8 +11,8 @@
 #define HTONS(n) (uint16_t)((((uint16_t) (n)) << 8) | (((uint16_t) (n)) >> 8))
 
 struct __attribute__ ((packed)) eth_hdr {
-  uint8_t dest[8];
-  uint8_t src[8];
+  uint8_t dest[6];
+  uint8_t src[6];
   uint16_t type;
 };
 
