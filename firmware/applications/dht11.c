fb6503ec8c64baa2098d6e9ee30b2143f179da9d
Martin Heistermann <mh-git@sponc.de>
2012-06-22 00:29:53 +0200

    dht11 app works fine, sending udp packets to my server

--- firmware/applications/dht11.c
+++ firmware/applications/dht11.c
@@ -16,7 +16,7 @@ struct __attribute__((packed)) my_payload {
     int8_t dht11_status;
     uint8_t temperature;
     uint8_t humidity;
-    uint32_t voltage;
+    uint16_t voltage;
 };
 
 struct __attribute__((packed)) packet {
@@ -26,37 +26,72 @@ struct __attribute__((packed)) packet {
     struct my_payload payload;
 };
 
+uint16_t checksum(uint16_t startval, void *addr, uint16_t bytes)
+{ // code based on code from rfc 1071
+    /* Compute Internet Checksum for "bytes" bytes
+     *         beginning at location "addr".
+     */
+    uint32_t sum = startval;
+
+    while( bytes > 1 )  {
+        /*  This is the inner loop */
+        sum += * ((uint16_t*) addr);
+        bytes -= 2;
+        addr += 2;
+    }
+
+    /*  Add left-over byte, if any */
+    if( bytes > 0 )
+        sum += * (unsigned char *) addr;
+
+    /*  Fold 32-bit sum to 16 bits */
+    while (sum>>16)
+        sum = (sum & 0xffff) + (sum >> 16);
+
+    return ~(uint16_t)sum;
+}
+
+/*********** start  uip code *************/
+
 void main_dht11(void) {
     struct packet *p = (struct packet*) ether_buf;
     struct my_payload *pl = &p->payload;
+    const uint16_t udplen = sizeof(struct udp_hdr) + sizeof(struct my_payload);
 
+    //uint16_t chksum = 0;
     uint16_t ipid = 0;
-    int res;
+    int res = 0;
     struct dht11_data data;
 
     dht11_init();
     initMAC();
 
+    // LCD
+    lcdClear();
+    lcdPrintln ("dht11-ip sensor");
+    lcdRefresh();
+
 
     ether_len = sizeof(*p);
 
-    memcpy(p->eth.dest, "\0foobar", 6);
-    memcpy(&p->eth.src, "\0foobas", 6);
-    p->eth.type = HTONS(UIP_ETHTYPE_IP);
+    memcpy(p->eth.dest, "\x1c\xbd\xb9\xa9\xd6\x4e", 6);
+    memcpy(&p->eth.src, "\0CC_CP", 6);
+    p->eth.type = HTONS16(UIP_ETHTYPE_IP);
     p->ip.vhl = 0x45;
     p->ip.tos = 0;
     p->ip.len[0] = 0;
-    p->ip.len[1] = ether_len;
+    p->ip.len[1] = ether_len - sizeof(struct eth_hdr);
     p->ip.ipoffset[0] = 0;
     p->ip.ipoffset[1] = 0;
     p->ip.ttl = 0xf0;
     p->ip.proto = UIP_PROTO_UDP;
-    memcpy(p->ip.srcipaddr, "AAAA", 4);
-    memcpy(p->ip.destipaddr, "BBBB", 4);
+    p->ip.srcipaddr = HTONS32(0x0a0a00aa);
+    //p->ip.destipaddr = HTONS32(0x0a0a0003);
+    p->ip.destipaddr = HTONS32(0xb009b264);
 
     p->udp.srcport = 0xdead;
     p->udp.destport = 0xbeef;
-    p->udp.udplen = HTONS(sizeof(p->udp) + sizeof(struct my_payload));
+    p->udp.udplen = HTONS16(udplen);
 
     p->payload.id = 0;
 
@@ -64,9 +99,15 @@ void main_dht11(void) {
     data.humidity = 0xff;
 
     while (true) {
+        lcdRefresh();
+
         VoltageCheck();
+
+        lcdRefresh();
         res = dht11_read(&data);
 
+        lcdClear();
+        lcdRefresh();
         pl->id++;
         pl->dht11_status = res;
         pl->temperature = data.temperature;
@@ -77,8 +118,16 @@ void main_dht11(void) {
         p->ip.ipid[1] = ipid & 0xff;
         ipid++;
 
-        p->ip.ipchksum = 0; // TODO XXX
-        p->udp.udpchksum = 0; // TODO ggf
+        p->ip.ipchksum = 0;
+        p->ip.ipchksum = checksum(0, &p->ip, sizeof(struct ip_hdr));
+        p->udp.udpchksum = 0;
+        /* TODO
+        chksum = checksum(HTONS16(udplen) + UIP_PROTO_UDP, &p->udp, udplen);
+        p->udp.udpchksum = checksum(chksum, &p->ip.srcipaddr, sizeof(uint32_t) * 2);
+
+        if (p->udp.udpchksum == 0)
+            p->udp.udpchksum = 0xffff;
+            */
 
         MACWrite();
 
